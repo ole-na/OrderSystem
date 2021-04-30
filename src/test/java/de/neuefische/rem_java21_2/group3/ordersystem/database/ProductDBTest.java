ec6bf4e9d8f3c47286cd175159b5be7dfa80e0d7
Christian Riedel <riechr@it-profi-hamburg.de>
2021-04-30 11:34:49 +0200

    add productDbTest

---
+++ src/test/java/de/neuefische/rem_java21_2/group3/ordersystem/database/ProductDBTest.java
@@ -0,0 +1,16 @@
+package de.neuefische.rem_java21_2.group3.ordersystem.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDBTest {
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void get() {
+    }
+}
