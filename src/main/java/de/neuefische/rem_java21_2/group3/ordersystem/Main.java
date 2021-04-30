b35b11a8778824ed2419670741446b335a20c4df
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-04-30 12:47:06 +0200

    Add Main + Add MainTest

---
+++ src/main/java/de/neuefische/rem_java21_2/group3/ordersystem/Main.java
@@ -0,0 +1,22 @@
+package de.neuefische.rem_java21_2.group3.ordersystem;
+
+import de.neuefische.rem_java21_2.group3.ordersystem.model.Order;
+import de.neuefische.rem_java21_2.group3.ordersystem.model.Product;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public Map<String, Product> listProducts() {
+        return null;
+    }
+
+    public void orderProducts(Product[] products) {
+
+    }
+
+    public List<Order> listOrders() {
+        return null;
+    }
+}
---
+++ src/test/java/de/neuefische/rem_java21_2/group3/ordersystem/MainTest.java
@@ -0,0 +1,20 @@
+package de.neuefische.rem_java21_2.group3.ordersystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void listProducts() {
+    }
+
+    @Test
+    void orderProducts() {
+    }
+
+    @Test
+    void listOrders() {
+    }
+}
