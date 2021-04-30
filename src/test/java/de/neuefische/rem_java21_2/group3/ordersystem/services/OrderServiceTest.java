a37a607a09a8148784302130bf5f1df746d88a5c
Christian Riedel <riechr@it-profi-hamburg.de>
2021-04-30 18:01:33 +0200

    add method addOrders in OrderServiceTest

--- src/test/java/de/neuefische/rem_java21_2/group3/ordersystem/services/OrderServiceTest.java
+++ src/test/java/de/neuefische/rem_java21_2/group3/ordersystem/services/OrderServiceTest.java
@@ -1,7 +1,11 @@
 package de.neuefische.rem_java21_2.group3.ordersystem.services;
 
 import de.neuefische.rem_java21_2.group3.ordersystem.model.Order;
+import de.neuefische.rem_java21_2.group3.ordersystem.model.Product;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +14,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
 
-    @Test
-    void addOrder() {
+    private static Arguments[] provideTestAddOrderArguments(){
+        return new Arguments[]{
+                Arguments.of((Object) new Product[]{}),
+                Arguments.of((Object) new Product[]{new Product("1", "Teddy")}),
+                Arguments.of((Object) new Product[]{new Product("-1", "Teddy")})
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestAddOrderArguments")
+    void addOrder(Product[] givenProducts) {
+
+        //Given
+        OrderService testOrderService = new OrderService();
+        Order newOrder = new Order("1", givenProducts);
+        List<Order> expected = List.of(newOrder);
+
+        //When
+        testOrderService.addOrder(givenProducts);
+        List<Order> actual = testOrderService.listOrders();
+
+        //Then
+        assertEquals(expected, actual);
+
     }
 
     @Test
