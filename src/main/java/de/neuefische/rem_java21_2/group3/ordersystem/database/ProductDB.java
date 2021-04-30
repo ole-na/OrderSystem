78912dc1870e191b317504d8ff2a2f6c7e2eb814
Christian Riedel <riechr@it-profi-hamburg.de>
2021-04-30 11:28:46 +0200

    add productdb method signature

---
+++ src/main/java/de/neuefische/rem_java21_2/group3/ordersystem/database/ProductDB.java
@@ -0,0 +1,18 @@
+package de.neuefische.rem_java21_2.group3.ordersystem.database;
+import de.neuefische.rem_java21_2.group3.ordersystem.model.Product;
+
+import java.util.*;
+
+public class ProductDB {
+
+    private Map<Integer, Product> productMap = new HashMap<>();
+
+    public Map list(){
+        return null;
+    }
+
+    public Product get(String productId){
+        return null;
+    }
+
+}
