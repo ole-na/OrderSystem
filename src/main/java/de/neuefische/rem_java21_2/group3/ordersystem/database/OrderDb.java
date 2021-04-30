2d98ccd9d52b32758481d1097b4e4ea8474c5c3b
Christian Riedel <riechr@it-profi-hamburg.de>
2021-04-30 11:54:32 +0200

    add OrderDb with methods and OrderDbTest

---
+++ src/main/java/de/neuefische/rem_java21_2/group3/ordersystem/database/OrderDb.java
@@ -0,0 +1,21 @@
+package de.neuefische.rem_java21_2.group3.ordersystem.database;
+import de.neuefische.rem_java21_2.group3.ordersystem.model.Order;
+
+import java.util.*;
+
+public class OrderDb {
+
+    private List<Order> orderList = new ArrayList<>();
+
+    public List<Order> list(){
+        return null;
+    }
+
+    public Order get(String orderId){
+        return null;
+    }
+
+    public void add(Order newOrder){
+    }
+
+}
---
+++ src/test/java/de/neuefische/rem_java21_2/group3/ordersystem/database/OrderDbTest.java
@@ -0,0 +1,20 @@
+package de.neuefische.rem_java21_2.group3.ordersystem.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderDbTest {
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void add() {
+    }
+}
